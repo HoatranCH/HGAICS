0588ad97dddc669216872d6a99412ad9192939af
HoatranCH <62659390+HoatranCH@users.noreply.github.com>
2021-03-11 14:16:33 +0700

    Delete RouletteWheelSelection.m

--- RouletteWheelSelection.m
+++
@@ -1,9 +0,0 @@
-function i=RouletteWheelSelection(P)
-
-    r=rand;      % Tao ngau nghien R 
-    
-    c=cumsum(P); % Cong tong theo cot cua P
-    
-    i=find(r<=c,1,'first'); % Tim 1 so sao cho r<=c, 
-
-end
